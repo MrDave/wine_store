79973aa51a374ea43d26125d82e1d0f534d2519d
MrDave <davidschnoll@gmail.com>
2023-08-18 18:48:39 +0300

    Automatic category and wine from inventory dict

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 import datetime
 import pandas
+from excel_parser import get_inventory
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -10,7 +11,7 @@ env = Environment(
 )
 
 template = env.get_template('template.html')
-
+inventory = get_inventory("wine2.xlsx")
 founding_date = datetime.date(year=1920, month=1, day=1)
 company_age = datetime.date.today()-founding_date
 company_years = company_age.days//365
@@ -20,12 +21,12 @@ elif company_years % 10 in range(2, 5) and company_years not in range(12, 15):
     age_text = "года"
 else:
     age_text = "лет"
-    
+
 wines = pandas.read_excel("wine.xlsx").to_dict(orient="records")
 
 rendered_page = template.render(
     company_years=f"Уже {company_years} {age_text} с вами",
-    wines=wines
+    inventory=inventory
 )
 
 with open('index.html', 'w', encoding="utf8") as file:
